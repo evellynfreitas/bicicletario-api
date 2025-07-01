7a08d09bed439ab92a8a88777557768f845fa3d6
janainamendes <janainamendes@edu.unirio.br>
2025-07-01 00:27:25 -0300

    Alterando main

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from controllers import hello_controller
 
 app = FastAPI(
   title="bicicletario-api 🚴",
   description=("Sistema de Controle de Bicicletário"),
   version="1.0.0"
 )
-app.include_router(hello_controller.router)
